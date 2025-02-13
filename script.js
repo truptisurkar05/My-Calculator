8218fd97018caf9f4fa5e5125d2f9633dc84cf86
truptisurkar05 <truptisurkar53@gmail.com>
2025-02-12 22:27:23 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function myclear()
 {
     a.value ="";
 }
+function myTheme(no)
+{   
+    var body = document.getElementById("body1");
+    if(no == '1')
+    {        
+        body.style.backgroundImage = 'url(image2.jpg)';       
+    }          
+    else if(no == '2')
+    {        
+        body.style.backgroundImage = 'url(imagenature.jpg)';        
+    }
+    else if(no == '3')
+    {        
+        body.style.backgroundImage = 'url(image3.jpg)';        
+    }
+    else if(no == '4')
+    {        
+        body.style.backgroundImage = 'url(image5.jpg)';        
+    }
+       
+}
